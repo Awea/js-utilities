fec0cf42620b9cf9acb2c04ad91c37cba974ea5a
Awea <aweaoftheworld@gmail.com>
2019-07-16 13:24:13 +0200

    :sparkles: Add documentHeight to src/responsive

--- src/responsive.js
+++ src/responsive.js
@@ -12,3 +12,18 @@ function documentWidth() {
   );
 }
 module.exports.documentWidth = documentWidth;
+
+/**
+ * Get the viewport height.
+ * @returns {number} Viewport height
+ */
+function documentHeight() {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  );
+}
+module.exports.documentHeight = documentHeight;
